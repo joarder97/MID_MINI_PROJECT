6201ead7d0b5c1e5caa96b53896d5dfd81c4ebb3
joarder97 <masudul.pallab99@gmail,com>
2020-08-10 13:19:25 +0600

    loginAdded

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <legend>LOGIN</legend>
+            <table>
+                <tr>
+                    <td>Id</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="id" value=""></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                </tr>
+                <tr>
+                    <td><input type="password" name="password" value=""></td>
+                </tr>
+                <tr>
+                    <td><hr></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Login" name="logBtn">
+                        <a href="reg.php">Register</a>
+                    </td>
+                </tr>
+
+
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
